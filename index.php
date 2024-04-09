cdcc294732b742331e5e40b6971eb1f37c1fcf2f
Sage Markwardt <sagemarkwardt@gmail.com>
2024-04-09 13:29:13 -0400

    add controller

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+// 328/hello-fat-free/index.php
+// this is my CONTROLLER
+
+// turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+// require the autoload file
+require_once ('vendor/autoload.php');
+
+// instantiate the F3 framework Base class
+$f3 = Base::instance();
+
+// define a default route
+$f3->route('GET /', function() {
+    echo '<h1>Hello Fat-Free!</h1>';
+});
+
+// Run fat-free
+$f3->run();
